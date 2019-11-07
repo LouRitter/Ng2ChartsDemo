2820dc85cd6ae349c47214a844dc9e9b06ec5c89
Lou <lritter@hybridhealth.com>
2019-11-07 10:48:11 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
+import { MyLineChartComponent } from './my-line-chart/my-line-chart.component';
+import { MyBarChartComponent } from './my-bar-chart/my-bar-chart.component';
+import { MyPieChartComponent } from './my-pie-chart/my-pie-chart.component';
+import { MyScatterChartComponent } from './my-scatter-chart/my-scatter-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyLineChartComponent,
+    MyBarChartComponent,
+    MyPieChartComponent,
+    MyScatterChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
